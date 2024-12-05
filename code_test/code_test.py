99862323c7d1e7f361fbb9b2a189816c207d5b20
sptbo <sptbo@163.com>
2024-12-05 17:14:13 +0800

    第一次提交。

--- code_test/code_test.py
+++ code_test/code_test.py
@@ -1,19 +1,14 @@
-from http import HTTPStatus
-import dashscope
+import os
+from openai import OpenAI
 
-
-messages = [
-    {'role': 'system', 'content': 'You are a helpful assistant.'},
-    {'role': 'user', 'content': '请编写一个Python函数 find_prime_numbers，该函数接受一个整数 n 作为参数，并返回一个包含所有小于 n 的质数（素数）的列表。质数是指仅能被1和其自身整除的正整数，如2, 3, 5, 7等。请不要输出代码之外的内容。'}]
-response = dashscope.Generation.call(
-    model='qwen2.5-coder-32b-instruct',
-    messages=messages,
-    result_format='message',  
+client = OpenAI(
+    api_key=os.getenv("DASHSCOPE_API_KEY"), # 如果您没有配置环境变量，请在此处用您的API Key进行替换
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",  # 填写DashScope服务的base_url
 )
-if response.status_code == HTTPStatus.OK:
-    print(response.output.choices[0].message.content)
-else:
-    print('Request id: %s, Status code: %s, error code: %s, error message: %s' % (
-        response.request_id, response.status_code,
-        response.code, response.message
-    ))
+completion = client.chat.completions.create(
+    model="qwen-coder-plus",
+    messages=[
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': '请编写一个Python函数 find_prime_numbers，该函数接受一个整数 n 作为参数，并返回一个包含所有小于 n 的质数（素数）的列表。质数是指仅能被1和其自身整除的正整数，如2, 3, 5, 7等。不要输出非代码的内容。'}],
+    )
+print(completion.choices[0].message.content)
